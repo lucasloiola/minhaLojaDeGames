e0681f0df6b6f504dd825ff759a781a41e3af9b3
Lucas <lucasloiolaalmeida@gmail.com>
2021-02-24 12:58:26 -0300

    Criação de camadas

---
+++ src/main/java/org/generation/minhaLojaDeGames/repository/CategoriaRepository.java
@@ -0,0 +1,10 @@
+package org.generation.minhaLojaDeGames.repository;
+
+import java.util.List;
+
+import org.generation.minhaLojaDeGames.model.Categoria;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CategoriaRepository extends JpaRepository<Categoria, Long>{
+	public List<Categoria> findAllByDescricaoContainingIgnoreCase(String descricao);
+}
